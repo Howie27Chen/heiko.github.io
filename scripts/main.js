6634a1f68d1f916e2ba49b84c966f4448f30427d
HeikoChen <15820583113@163.com>
2020-12-27 17:17:37 +0800

    add(js): 首次尝试 js 给网页提供基本交互

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字');
+    if (!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = myName + '欢迎来到 Heiko\'s Blog';
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = storedName + '欢迎来到 Heiko\'s Blog';
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/pic_heiko_freshman.png') {
+        myImage.setAttribute('src', 'images/pic_heiko_senior.png');
+    } else {
+        myImage.setAttribute('src', 'images/pic_heiko_freshman.png');
+    }
+}
